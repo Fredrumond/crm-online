0c4d1786c42a5b7ac6ba3ed9b1d87582b32a3892
Frederico Drumond <fredrumond@gmail.com>
2017-01-24 21:04:57 -0200

    [UP] Lembrete

---
+++ novo/view/lembrete/modal-lembrete.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="novo-lembrete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Adicionar Lembrete</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="">Titulo</label>
+            <input type="text" class="form-control" id="" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Data</label>
+            <input type="text" class="form-control" id="" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Informação</label>
+            <input type="text" class="form-control" id="" placeholder="">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-success">Salvar</button>
+      </div>
+    </div>
+  </div>
+</div>
